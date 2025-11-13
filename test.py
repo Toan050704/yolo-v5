2736753b3be43414f4b41bfa08ab9634e3f68a49
UltralyticsAssistant <web@ultralytics.com>
2025-11-13 05:39:55 +0000

    Auto-format by https://ultralytics.com/actions

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-import cv2
-import os
 from ultralytics import YOLO
 
 input_path = r"D:\BCTT\yolov5\video_demo\NKKN-VoThiSau 2017-07-18_08_00_00_000.asf"
 
 model = YOLO(r"D:\BCTT\yolov5\runs\detect\train_customdata\weights\best.pt")
 results = model.predict(input_path, conf=0.4, save=True, show=True, imgsz=800)
-
-
